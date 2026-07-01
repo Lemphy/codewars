8400e886c8bc136ea09d5056c20a036ef167b67f
Михайло Олефіренко <79942127+Lemphy@users.noreply.github.com>
2026-07-01 19:00:38 +0300

    Delete 8 kuy directory

--- 8 kuy/Color Ghost.py
+++
@@ -1,8 +0,0 @@
-# https://www.codewars.com/kata/53f1015fa9fe02cbda00111a
-
-import random
-
-class Ghost:
-    def __init__(self):
-        colors = ['white', 'yellow', 'purple', 'red']
-        self.color = random.choice(colors)
